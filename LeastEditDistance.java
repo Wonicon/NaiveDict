1abe5968bc108c82a6306034fb6803350cc16aa0
Wonicon <conciselove@outlook.com>
2016-10-31 17:16:55 +0800

    Fix bug of least edit distance

--- LeastEditDistance.java
+++ LeastEditDistance.java
@@ -14,8 +14,10 @@ public class LeastEditDistance {
 
     for (int i = 1; i <= second.length(); i++) {
       for (int j = 1; j <= first.length(); j++) {
-        m[i][j] = Math.min(m[i - 1][j], Math.min(m[i][j - 1], m[i - 1][j - 1]))
-                  + (first.charAt(j - 1) == second.charAt(i - 1) ? 0 : 1);
+        m[i][j] = Math.min(
+            Math.min(m[i - 1][j], m[i][j - 1]) + 1,
+            m[i - 1][j - 1] + (first.charAt(j - 1) == second.charAt(i - 1) ? 0 : 1)
+        );
       }
     }
 
