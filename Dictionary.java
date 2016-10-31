3ea279d47b5a42fdff5d7cd59831ee16748db23d
Wonicon <conciselove@outlook.com>
2016-10-31 18:41:07 +0800

    Add a text area to display details

--- Dictionary.java
+++ Dictionary.java
@@ -19,25 +19,41 @@ public class Dictionary extends Application {
 
   private String selectedEntry = null;
 
+  TextArea textArea;
+
+  private void updateInfo(String word) {
+    Entry entry = index.getNext(word).getEntry();
+    textArea.setText(
+      entry.getWord() + "\n"
+      + entry.getPronunciation() + "\n"
+      + entry.getDescription()
+    );
+  }
+
   @Override
   public void start(Stage primaryStage) throws Exception {
     // Init resources
+
     words = Entry.getEntriesFromDictionary("dictionary.txt");
     index = TrieTree.createTree(words);
     prefixAssociation = new PrefixVisitor(index);
 
     // Init UI elements
-    TextField textField = new TextField("Type Me");
 
+    TextField textField = new TextField("Type Me");
     ListView<String> listView = new ListView<>();
+    textArea = new TextArea("Hello");
+
+    // Bind list items to entry list.
     listView.setItems(entries);
 
     // When we select an item in the side bar, update the text in the text field.
-    ///  @todo update detail info panel when selected.
+    /// @todo update detail info panel when selected.
     listView.getSelectionModel().selectedItemProperty().addListener((o, old_, new_) -> {
       if (new_ != null) {
         selectedEntry = new_;
         textField.setText(new_);
+        updateInfo(new_);
       }  // Otherwise the selection is missing.
     });
 
@@ -55,6 +71,7 @@ public class Dictionary extends Application {
       Entry ent = root == null ? null : root.getEntry();
       if (ent != null) {
         System.out.println(ent.getDescription());
+        updateInfo(ent.getWord());
       } else {
         Timer t = new Timer();
 
@@ -72,15 +89,22 @@ public class Dictionary extends Application {
       }
     });
 
-    VBox stackPane = new VBox();
-    stackPane.getChildren().add(textField);
-    stackPane.getChildren().add(listView);
-    stackPane.getPadding();
-    Group root = new Group(stackPane);
-    Scene scene = new Scene(root, 400, 300);
+    // Init layouts
+
+    VBox vbox = new VBox();
+    vbox.getChildren().addAll(textField, listView);
+    vbox.getPadding();
+
+    BorderPane borderPane = new BorderPane();
+    borderPane.setLeft(vbox);
+    borderPane.setRight(textArea);
+
+    Group root = new Group(borderPane);
+    Scene scene = new Scene(root, 400, 400);
     primaryStage.setTitle("Dictionary");
     primaryStage.setScene(scene);
     primaryStage.show();
+    primaryStage.setResizable(false);
   }
 
   public static void main(String[] args) {
